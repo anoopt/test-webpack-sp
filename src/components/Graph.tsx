56656e004582ea5cc327bea16edc8ea2d94d445d
Anoop Tatti <anoop.tatti@gmail.com>
2017-10-17 10:50:17 +0100

    minor change

--- src/components/Graph.tsx
+++ src/components/Graph.tsx
@@ -2,30 +2,56 @@ import * as React from "react";
 import * as $ from "jquery";
 
 export interface GraphProps { }
-export interface IGraphState { yourName: string }
+export interface IGraphState { 
+    displayName: string;
+    givenName: string;
+    mobilePhone: string;
+ }
+
+export interface IDisplayProps {
+    displayName: string;
+    givenName: string;
+    mobilePhone: string;
+}
+
 export class Graph extends React.Component<GraphProps, IGraphState> {
 
     constructor(props: GraphProps){
         super(props);
         this.state = {
-            yourName: ""
+            displayName: "",
+            givenName: "",
+            mobilePhone: ""
         };
     }
 
     public componentDidMount(): void {
-        this.getName().then(r => {
+        /* this.getName().then(r => {
             this.setState({
                 yourName: r
+            })
+         }); */
+
+         this.getDetails().then(r => {
+            this.setState({
+                displayName: r.displayName,
+                givenName: r.givenName,
+                mobilePhone: r.mobilePhone
             })
          });
          
       }
 
     public render(): React.ReactElement<GraphProps> {
-        return <h1>Your name is {this.state.yourName}!</h1>;
+        return (
+        <div>
+            <p>Your Name is {this.state.displayName}!</p>
+            <p>Your Given Name is {this.state.givenName}!</p>
+            <p>Your Mobile Number is {this.state.mobilePhone}!</p>
+        </div>);
     }
 
-    private async getName(): Promise<any> {
+    /* private async getName(): Promise<string> {
         const msGraphToken = await this.getMSGraphAccessToken();
         let dfd: any = $.Deferred();
         $.ajax({
@@ -45,10 +71,30 @@ export class Graph extends React.Component<GraphProps, IGraphState> {
             }
         });
         return dfd.promise();
+    } */
+
+    private async getDetails(): Promise<IDisplayProps> {
+        const msGraphToken = await this.getMSGraphAccessToken();
+        let dfd: any = $.Deferred();
+        $.ajax({
+            url: "https://graph.microsoft.com/v1.0/me/",
+            headers: {
+                "Accept": "application/json",
+                "Authorization": "Bearer " + msGraphToken
+            },
+            success: function (data) {
+                console.log(data);
+                dfd.resolve(data);
+            },
+            error: function (jqxr, errorCode, errorThrown) {
+                console.log(jqxr.responseText);
+                dfd.reject("error");
+            }
+        });
+        return dfd.promise();
     }
 
-
-    private getMSGraphAccessToken(): Promise<any> {
+    private getMSGraphAccessToken(): Promise<string> {
         
         let dfd: any = $.Deferred();
 
